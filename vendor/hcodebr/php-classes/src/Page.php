0aa3ab242c170b7cb8ac169ae149d67d2ce20a3e
LuisBonfa <luis.araujo01@fatec.sp.gov.br>
2018-08-03 20:38:01 -0300

    crud com erro

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,6 +9,8 @@ class Page{
   private $tpl;
   private $options = [];
   private $defaults = [
+    "header"=>true,
+    "footer"=>true,
     "data"=>[]
   ];
 
@@ -24,7 +26,10 @@ class Page{
   	Tpl::configure( $config );
     $this->tpl = new Tpl;
     $this->setData($this->options["data"]);
-    $this->tpl->draw("header");
+
+    if($this->options["header"] === true){
+      $this->tpl->draw("header");
+    }
 
   }
 
@@ -34,7 +39,10 @@ class Page{
   }
 
   public function __destruct(){
-    $this->tpl->draw("footer");
+    if($this->options["footer"] === true){
+      $this->tpl->draw("footer");
+    }
+
   }
 
   private function setData($data = array()){
